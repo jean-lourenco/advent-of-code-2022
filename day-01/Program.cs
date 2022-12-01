118eab153db49593083cb1edf053e5783623b753
Jean Lourenço <jeancarlos.bnu@gmail.com>
2022-12-01 19:16:15 -0300

    Solve day 01

---
+++ day-01/Program.cs
@@ -0,0 +1,13 @@
+﻿var nl = Environment.NewLine;
+var calories = File.ReadAllText("input.txt")
+    .Split(nl + nl)
+    .Select(x => x.Split(nl).Select(int.Parse).Sum())
+    .OrderByDescending(x => x)
+    .Take(3)
+    .ToArray();
+
+Console.WriteLine($"Result 1: {calories.First()}");
+Console.WriteLine($"Result 2: {calories.Sum()}");
+
+System.Diagnostics.Debug.Assert(calories.First() == 67633);
+System.Diagnostics.Debug.Assert(calories.Sum() == 199628);
